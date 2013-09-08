2c0876ddd8a77c98d0aa533aa9194c28b5fc7c95
Dan Gunter <dkgunter@lbl.gov>
2013-09-07 18:26:05 -0700

    updated for wrappers.py

--- examples/pymongo_example_2.py
+++ examples/pymongo_example_2.py
@@ -1,5 +1,5 @@
 """
-Simple example of usage with pymongo, from the README
+Simple example of usage with pymongo, with drop-in MongoClient
 """
 __author__ = 'Dan Gunter <dkgunter@lbl.gov>'
 __date__ = '9/6/13'
@@ -12,7 +12,7 @@ except ImportError:
     sys.exit(1)
 import smoqe
 
-client = pymongo.MongoClient()
+client = smoqe.MongoClient()
 coll = client.test.test
 # create/populate DB
 coll.remove()
@@ -20,11 +20,9 @@ coll.insert([{'n': 1, 'a': 1, 'b': 2, 'c': 'hello'},
              {'n': 2, 'a': -1, 'b': 3.14, 'c': 3},
              {'n': 3, 'a': 2, 'b': 99, 'c': 4}])
 # perform queries
-q = smoqe.to_mongo("a > 0 and b > 0 and c type string")
-r = coll.find(q)
+r = coll.find("a > 0 and b > 0 and c type string")
 print(r[0]['n'])
 # prints: 1
-q = smoqe.to_mongo("a < 0 or c = 4")
-r = coll.find(q)
+r = coll.find("a < 0 or c = 4")
 print("{},{}".format(r[0]['n'], r[1]['n']))
 # prints: 2,3
