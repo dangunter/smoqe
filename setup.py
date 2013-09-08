3e77bebd3bc8b9f2befdff7509545a51e3bd313c
Dan Gunter <dkgunter@lbl.gov>
2013-09-07 18:26:47 -0700

    fixed name to smoque with lowercase s

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(
-    name="Smoqe",
+    name="smoqe",
     version="0.1",
     packages=find_packages(),
 # TBD:
